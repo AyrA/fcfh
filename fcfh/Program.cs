b0ed99ce15c90555f5cdfb7216859272dc265953
Kevin Gut <kevin.gut@bitm.sg>
2017-08-14 16:55:56 +0200

    Add project files.

---
+++ fcfh/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace fcfh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
